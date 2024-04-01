34ceba96ca347afa60d510df678ad76887f2f83c
moriisaac <wesongamori@gmail.com>
2024-04-02 05:20:37 +1100

    Updates:New Commits

--- PMS/Medicine/admin.py
+++ PMS/Medicine/admin.py
@@ -1,3 +1,13 @@
-from django.contrib import admin
+# In medicine/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import Medicine
+
+class MedicineAdmin(admin.ModelAdmin):
+    list_display = ('name', 'stage', 'expiry_date', 'price', 'supplier_name', 'medicine_type')
+    list_filter = ('medicine_type', 'expiry_date')
+    search_fields = ('name', 'stage', 'supplier_name')
+    ordering = ('-expiry_date',)
+    list_per_page = 25
+
+admin.site.register(Medicine, MedicineAdmin)
--- PMS/Medicine/models.py
+++ PMS/Medicine/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Medicine(models.Model):
+    MEDICINE_TYPES = (
+        ('Chicks', 'Chicks'),
+        ('Broilers', 'Broilers'),
+        ('Layers', 'Layers'),
+        ('Other', 'Other')
+    )
+
+    name = models.CharField(max_length=100)
+    stage = models.CharField(max_length=100)
+    expiry_date = models.DateField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    supplier_name = models.CharField(max_length=100)
+    medicine_type = models.CharField(max_length=100, choices=MEDICINE_TYPES)
+
+    def __str__(self):
+        return self.name
