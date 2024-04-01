a382f7ae86377fa4af1e33a5347bb98856a0adb7
moriisaac <wesongamori@gmail.com>
2024-04-02 05:12:14 +1100

    Updates:New Commits

--- PMS/Feed/admin.py
+++ PMS/Feed/admin.py
@@ -1,14 +1,19 @@
+# In feed/admin.py
 
-
-# Register your models here.
 from django.contrib import admin
 from .models import Feed
 
 class FeedAdmin(admin.ModelAdmin):
-    list_display = ('name', 'type', 'expiry_date', 'administration_method', 'price', 'supplier_name','feeding_time','quantity_used')
-    list_filter = ('type', 'expiry_date')
+    list_display = ('name', 'type', 'expiry_date', 'administration_method', 'price', 'supplier_name', 'feeding_time', 'quantity_bought', 'quantity_used', 'total_quantity_remaining', 'is_quantity_below_threshold')
+    list_filter = ('type', 'expiry_date', 'administration_method', 'supplier_name', 'feeding_time')
     search_fields = ('name', 'type', 'supplier_name')
-    ordering = ('expiry_date',)
+    ordering = ('-feeding_time',)
     list_per_page = 25
+
+    def is_quantity_below_threshold(self, obj):
+        return obj.is_quantity_below_threshold
+
+    is_quantity_below_threshold.boolean = True
+    is_quantity_below_threshold.short_description = 'Below Threshold'
 
 admin.site.register(Feed, FeedAdmin)
--- PMS/Feed/models.py
+++ PMS/Feed/models.py
@@ -1,3 +1,5 @@
+# In feed/models.py
+
 from django.db import models
 
 class Feed(models.Model):
@@ -8,7 +10,18 @@ class Feed(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     supplier_name = models.CharField(max_length=100)
     feeding_time = models.DateTimeField()
+    quantity_bought = models.DecimalField(max_digits=10, decimal_places=2,default=10)
     quantity_used = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return self.name
+
+    @property
+    def total_quantity_remaining(self):
+        return self.quantity_bought - self.quantity_used
+
+    @property
+    def is_quantity_below_threshold(self):
+        if self.total_quantity_remaining < 0.2 * self.quantity_bought:
+            return True
+        return False
