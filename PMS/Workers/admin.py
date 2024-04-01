27f3e70cfcecaca9cbc92ec001ef7a0572d4a18a
moriisaac <wesongamori@gmail.com>
2024-04-02 05:27:49 +1100

    Updates:New Commits

--- PMS/Workers/admin.py
+++ PMS/Workers/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Worker
 
-# Register your models here.
+class WorkerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'contact_information', 'role', 'hourly_rate', 'date_employed', 'monthly_salary')
+    search_fields = ('name', 'contact_information', 'role')
+    list_per_page = 25
+
+admin.site.register(Worker, WorkerAdmin)
--- PMS/Workers/models.py
+++ PMS/Workers/models.py
@@ -1,3 +1,14 @@
+# In workers/models.py
+
 from django.db import models
 
-# Create your models here.
+class Worker(models.Model):
+    name = models.CharField(max_length=100)
+    contact_information = models.CharField(max_length=100)
+    role = models.CharField(max_length=100)
+    hourly_rate = models.DecimalField(max_digits=10, decimal_places=2)
+    date_employed = models.DateField()
+    monthly_salary = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
