0cc4bb31d7feb5ec47f4c712ca9d5b785a51baf1
moriisaac <wesongamori@gmail.com>
2024-04-02 05:04:03 +1100

    Updates:New Commits

--- PMS/Feed/admin.py
+++ PMS/Feed/admin.py
@@ -1,3 +1,14 @@
-from django.contrib import admin
+
 
 # Register your models here.
+from django.contrib import admin
+from .models import Feed
+
+class FeedAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'expiry_date', 'administration_method', 'price', 'supplier_name','feeding_time','quantity_used')
+    list_filter = ('type', 'expiry_date')
+    search_fields = ('name', 'type', 'supplier_name')
+    ordering = ('expiry_date',)
+    list_per_page = 25
+
+admin.site.register(Feed, FeedAdmin)
--- PMS/Feed/models.py
+++ PMS/Feed/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Feed(models.Model):
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+    expiry_date = models.DateField()
+    administration_method = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    supplier_name = models.CharField(max_length=100)
+    feeding_time = models.DateTimeField()
+    quantity_used = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
