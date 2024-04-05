8ac64265eae58569dd331c2a67b59527c44df81c
moriisaac <wesongamori@gmail.com>
2024-04-05 15:26:36 +1100

    Feat: Models for Manure

--- PMS/Manure/models.py
+++ PMS/Manure/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class ManureRemoval(models.Model):
+    date_of_removal = models.DateField()
+    destination = models.CharField(max_length=100)
+    quantity = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Manure removed on {self.date_of_removal}"
