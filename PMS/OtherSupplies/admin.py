b3bbd0cf348eb160f14db0c6efe74b183c30f02a
moriisaac <wesongamori@gmail.com>
2024-04-05 15:29:46 +1100

    Feat: Admin Model For Other Supplies

--- PMS/OtherSupplies/admin.py
+++ PMS/OtherSupplies/admin.py
@@ -1,3 +1,12 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import OtherSupply
+
+class OtherSupplyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'expiry_date', 'price', 'supplier_name')
+    list_filter = ('category', 'expiry_date')
+    search_fields = ('name', 'supplier_name')
+    date_hierarchy = 'expiry_date'
+    list_per_page = 25
+
+admin.site.register(OtherSupply, OtherSupplyAdmin)
