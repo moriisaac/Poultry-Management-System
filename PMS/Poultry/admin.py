d02e5aacf3d4fb30aae6393d54ed35f4a15a3572
moriisaac <wesongamori@gmail.com>
2024-04-05 19:30:38 +1100

    Feat: New Poultry App

--- PMS/Poultry/admin.py
+++ PMS/Poultry/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Poultry
 
-# Register your models here.
+class PoultryAdmin(admin.ModelAdmin):
+    list_display = ('poultry_house', 'poultry_type', 'total_quantity', 'kienyeji_grown_quantity', 'broilers_grown_quantity', 'layers_grown_quantity', 'chicks_type', 'kienyeji_chicks_quantity', 'broilers_chicks_quantity', 'layers_chicks_quantity')
+    list_filter = ('poultry_house', 'poultry_type', 'chicks_type')
+    search_fields = ('poultry_house__pen_number',)
+    ordering = ('-id',)
+    list_per_page = 25
+
+admin.site.register(Poultry, PoultryAdmin)
--- PMS/Poultry/models.py
+++ PMS/Poultry/models.py
@@ -3,28 +3,82 @@ from django.db import models
 
 class Poultry(models.Model):
     TYPE_CHOICES = (
+        ('kienyeji', 'Kienyeji'),
+        ('broilers', 'Broilers'),
+        ('layers', 'Layers'),
+    )
+    CHICKS_TYPE_CHOICES = (
         ('chicks_kienyeji', 'Chicks (Kienyeji)'),
         ('chicks_broilers', 'Chicks (Broilers)'),
         ('chicks_layers', 'Chicks (Layers)'),
     )
+    chicks_type = models.CharField(max_length=20, choices=CHICKS_TYPE_CHOICES, blank=True, null=True)
     poultry_house = models.ForeignKey('Farm.PoultryHouse', on_delete=models.CASCADE, to_field='pen_number')
-    type = models.CharField(max_length=20, choices=TYPE_CHOICES)
+    poultry_type = models.CharField(max_length=20, choices=TYPE_CHOICES,blank=True, null=True)
     total_quantity = models.PositiveIntegerField(default=0)
-    kienyeji_quantity = models.PositiveIntegerField(default=0)
-    broilers_quantity = models.PositiveIntegerField(default=0)
-    layers_quantity = models.PositiveIntegerField(default=0)
+    kienyeji_grown_quantity = models.PositiveIntegerField(default=0)
+    broilers_grown_quantity = models.PositiveIntegerField(default=0)
+    layers_grown_quantity = models.PositiveIntegerField(default=0)
+    kienyeji_chicks_quantity = models.PositiveIntegerField(default=0)
+    broilers_chicks_quantity = models.PositiveIntegerField(default=0)
+    layers_chicks_quantity = models.PositiveIntegerField(default=0)
+
+    def total_chicks_quantity(self):
+        chicks_quantity = 0
+        if self.chicks_type:
+            if self.chicks_type == 'chicks_kienyeji':
+                chicks_quantity += self.kienyeji_chicks_quantity
+            elif self.chicks_type == 'chicks_broilers':
+                chicks_quantity += self.broilers_chicks_quantity
+            elif self.chicks_type == 'chicks_layers':
+                chicks_quantity += self.layers_chicks_quantity
+        return chicks_quantity
 
     def save(self, *args, **kwargs):
-        if self.type == 'chicks_kienyeji':
-            self.kienyeji_quantity = self.total_quantity
-        elif self.type == 'chicks_broilers':
-            self.broilers_quantity = self.total_quantity
-        elif self.type == 'chicks_layers':
-            self.layers_quantity = self.total_quantity
+        if self.poultry_type:
+            if self.poultry_type == 'kienyeji':
+                self.kienyeji_grown_quantity = self.total_quantity
+            elif self.poultry_type == 'broilers':
+                self.broilers_grown_quantity = self.total_quantity
+            elif self.poultry_type == 'layers':
+                self.layers_grown_quantity = self.total_quantity
+
+        # Check if chicks_type is selected before updating chicks quantities
+        if self.chicks_type:
+            if self.chicks_type == 'chicks_kienyeji':
+                self.kienyeji_chicks_quantity = self.total_quantity
+            elif self.chicks_type == 'chicks_broilers':
+                self.broilers_chicks_quantity = self.total_quantity
+            elif self.chicks_type == 'chicks_layers':
+                self.layers_chicks_quantity = self.total_quantity
+
+        # Calculate the total quantity
+        total_quantity = (
+                self.kienyeji_grown_quantity + self.broilers_grown_quantity + self.layers_grown_quantity +
+                self.kienyeji_chicks_quantity + self.broilers_chicks_quantity + self.layers_chicks_quantity
+        )
+        self.total_quantity = total_quantity
+
         super().save(*args, **kwargs)
 
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['poultry_type'], name='unique_poultry_type'),
+        ]
+
+
+
     def __str__(self):
-        return f"{self.get_type_display()} - {self.total_quantity} ({self.poultry_house})"
+        if self.poultry_type:
+
+           return f"{self.poultry_type} - {self.total_quantity} ({self.poultry_house})"
+
+        elif self.chicks_type:
+            return f"{self.poultry_type} - {self.total_quantity} ({self.poultry_house})"
+        else:
+            return f"Poultry - {self.total_quantity} ({self.poultry_house})"
+
+
 
 
 class PoultryDeath(models.Model):
