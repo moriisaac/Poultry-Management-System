a79766f94f173d51e53cdd877a6fd9d890ccd492
moriisaac <wesongamori@gmail.com>
2024-04-05 17:53:28 +1100

    Feat: New Poultry App

--- PMS/Poultry/models.py
+++ PMS/Poultry/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Poultry(models.Model):
+    TYPE_CHOICES = (
+        ('chicks_kienyeji', 'Chicks (Kienyeji)'),
+        ('chicks_broilers', 'Chicks (Broilers)'),
+        ('chicks_layers', 'Chicks (Layers)'),
+    )
+    poultry_house = models.ForeignKey('Farm.PoultryHouse', on_delete=models.CASCADE, to_field='pen_number')
+    type = models.CharField(max_length=20, choices=TYPE_CHOICES)
+    total_quantity = models.PositiveIntegerField(default=0)
+    kienyeji_quantity = models.PositiveIntegerField(default=0)
+    broilers_quantity = models.PositiveIntegerField(default=0)
+    layers_quantity = models.PositiveIntegerField(default=0)
+
+    def save(self, *args, **kwargs):
+        if self.type == 'chicks_kienyeji':
+            self.kienyeji_quantity = self.total_quantity
+        elif self.type == 'chicks_broilers':
+            self.broilers_quantity = self.total_quantity
+        elif self.type == 'chicks_layers':
+            self.layers_quantity = self.total_quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.get_type_display()} - {self.total_quantity} ({self.poultry_house})"
+
