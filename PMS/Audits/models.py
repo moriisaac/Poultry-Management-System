edec27e28d32827e21c7fb9811dd16dfca231f8f
moriisaac <wesongamori@gmail.com>
2024-04-05 15:57:20 +1100

    Feat: Generatiom of Egg Production Report

--- PMS/Audits/models.py
+++ PMS/Audits/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class EggReport(models.Model):
+    app_name = models.CharField(max_length=100)
+    report_type = models.CharField(max_length=50)
+    criteria = models.CharField(max_length=255)
+    generated_at = models.DateTimeField(auto_now_add=True)
+    file_path = models.CharField(max_length=255)
