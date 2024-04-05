d1ddea1f167fab9b1df21a0399c64bcededb5344
moriisaac <wesongamori@gmail.com>
2024-04-05 15:30:35 +1100

    Feat: Admin Model For Manure Removal

--- PMS/Manure/admin.py
+++ PMS/Manure/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import ManureRemoval
 
-# Register your models here.
+class ManureRemovalAdmin(admin.ModelAdmin):
+    list_display = ('date_of_removal', 'destination', 'quantity')
+    list_filter = ('date_of_removal', 'destination')
+    search_fields = ('destination',)
+    date_hierarchy = 'date_of_removal'
+    list_per_page = 25
+
+admin.site.register(ManureRemoval, ManureRemovalAdmin)
